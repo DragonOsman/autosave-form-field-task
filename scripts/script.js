07c0b34103f0709ebae06cf59a17fa186cfe0b2b
Osman Zakir <osmanzakir90@hotmail.com>
2019-12-28 01:06:44 +0500

    fixed code

--- scripts/script.js
+++ scripts/script.js
@@ -2,33 +2,19 @@
 
 const area = document.getElementById("area");
 area.placeholder = "Write here";
-
-function autoSave() {
-  if (area.value) {
-    area.oninput = function() {
-      localStorage.setItem("area", area.value);
-    }
-    area.value = localStorage.getItem("area");
-  }
-}
-
-function onStorage() {
-  autoSave();
-}
-
-window.onstorage = onStorage;
-
-document.body.append(document.createElement("br"))
+document.body.append(document.createElement("br"));
 const clearButton = document.createElement("button");
 clearButton.textContent = "Clear";
-clearButton.onclick = clearValue;
 document.body.append(clearButton);
 
-function clearValue(event) {
-  if (event.target.value !== "Clear") {
-    return;
-  }
-
+clearButton.onclick = () => {
   localStorage.removeItem("area");
   area.value = "";
+}
+
+area.value = localStorage.getItem("area");
+area.oninput = () => {
+  if (area.value) {
+    localStorage.setItem("area", area.value);
+  }
 }
